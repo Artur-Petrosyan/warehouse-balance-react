57d9df0f0ce915898ae0710340cbdc7fbdde1544
Artur <arturpetrosyan199808@gmail.com>
2023-12-21 16:20:52 +0400

    feat(hooks): useNavigation
    add the JS Doc and change the 'path'

--- src/core/hooks/useNavigation.js
+++ src/core/hooks/useNavigation.js
@@ -1,14 +1,30 @@
+/**
+ * Returns a function that takes a path as an argument and returns a formatted path based on the input path.
+ * @function
+ * @name useNavigation
+ * @returns {string} - The formatted path.
+ * @throws {Error} - If the input path is not a string.
+ * @example
+ * const formattedPath = useNavigation()('Home'); // returns '/'
+ * const formattedPath2 = useNavigation()('Written Off'); // returns '/written-off'
+ * const formattedPath3 = useNavigation()('Contacts'); // returns '/contacts'
+ * const formattedPath4 = useNavigation()('Some Other Path'); // returns '/some-other-path'
+ *
+ * // Example of throwing an error
+ * const formattedPath5 = useNavigation()(123); // throws an error
+ */
+
 export const useNavigation = () => {
     return (path) => {
         const lowerPath = path.toLowerCase()
         if (lowerPath === 'home') {
             return '/'
         }
-        if (lowerPath === 'shows') {
-            return `${path}/1`
+        if (lowerPath === 'written off') {
+            return `written-off`
         }
-        if (lowerPath === 'people') {
-            return `${path}/1`
+        if (lowerPath === 'contacts') {
+            return 'contacts'
         }
         return lowerPath
     };
