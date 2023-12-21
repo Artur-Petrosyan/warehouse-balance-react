2327691264f9f70aebfcf0415ded970f2913e6be
Artur <arturpetrosyan199808@gmail.com>
2023-12-21 15:07:11 +0400

    feat(App): Header
    use the 'Header' component

--- src/app/App.js
+++ src/app/App.js
@@ -1,7 +1,16 @@
 import '../App.css';
+import { Header } from "../core";
+import { Router } from "./Providers/routes";
 
 const App = () => (
     <div className="App">
+        <Header navList={[
+            { id: 1, name: 'Home' },
+            { id: 2, name: 'Written off' },
+            { id: 3, name: 'Contacts' },
+        ]}></Header>
+        {Router()}
+
     </div>
 );
 
