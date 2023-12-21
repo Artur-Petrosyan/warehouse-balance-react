0b2b85d6e77694e84044490cbd91f11c6ff7ccdb
Artur <arturpetrosyan199808@gmail.com>
2023-12-21 14:36:20 +0400

    feat(craco): path
    create the 'paths' for 'core','pages','features','widgets','entities'

---
+++ craco.config.js
@@ -0,0 +1,17 @@
+const path = require('path')
+
+const resolvePath = (p) => path.resolve(__dirname,p)
+
+module.exports = {
+    webpack : {
+        port : '3000',
+        alias : {
+            src : resolvePath('./src'),
+            core: resolvePath('./src/core'),
+            pages : resolvePath('./src/pages'),
+            features: resolvePath('./src/features'),
+            widgets: resolvePath('./src/widgets'),
+            entities: resolvePath('./src/entities')
+        }
+    }
+}
