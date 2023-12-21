314e6611fadb69e03d8a86790bb4e9c56fdacd13
Artur <arturpetrosyan199808@gmail.com>
2023-12-21 16:19:50 +0400

    feat(pages): WrittenOffPage
    create 'WrittenOffPage' page

---
+++ src/pages/WrittenOff/ui/WrittenOffPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WrittenOffController = () => {
+    return (
+        <div>
+            WrittenOffController
+        </div>
+    );
+};
+
+export default WrittenOffController;
