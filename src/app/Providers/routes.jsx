18afe946e8a1de51944ae1c091d9eda326183c01
Artur <arturpetrosyan199808@gmail.com>
2023-12-21 16:21:44 +0400

    feat(routes): route
    create the new route '/written-off'

--- src/app/Providers/routes.jsx
+++ src/app/Providers/routes.jsx
@@ -1,15 +1,16 @@
 import {  Routes , Route } from "react-router-dom";
 
 import { lazy } from "react";
-import { LazyLoadWrapperHOC } from "core/lib/lazyLoadWrapperHOC";
+import { withLazyLoadWrapper } from "core/lib/withLazyLoadWrapper";
+import WrittenOffPage from "../../pages/WrittenOff/ui/WrittenOffPage";
 
 
-const Home = lazy(() => import('pages/Home/Home'))
+const Home = lazy(() => import('pages/Home/ui/HomePage'))
 export const Router = () => {
-
     return (
         <Routes>
-            <Route index={true} element={LazyLoadWrapperHOC(<Home />)} />
+            <Route index={true} element={withLazyLoadWrapper(<Home />)} />
+            <Route path='/written-off' element={withLazyLoadWrapper(<WrittenOffPage />)} />
         </Routes>
     )
 
