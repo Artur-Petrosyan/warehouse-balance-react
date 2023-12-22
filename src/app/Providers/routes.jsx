88b04667820112bafea4daabaffaada25729a22c
Artur <arturpetrosyan199808@gmail.com>
2023-12-22 14:52:06 +0400

    feat(routes): lazy
    use the 'withLazyLoadWrapper'

--- src/app/Providers/routes.jsx
+++ src/app/Providers/routes.jsx
@@ -5,13 +5,14 @@ import { withLazyLoadWrapper } from "core/lib/withLazyLoadWrapper";
 import { withMemo } from "core";
 
 const Home = lazy(() => import('pages/Home/ui/HomePage'))
+const WrittenOffPage = lazy(() => import('pages/WrittenOff/ui/WrittenOffPage'))
+
 export const Router = () => {
     return (
         <Routes>
             <Route index={true} element={withLazyLoadWrapper(<Home />)} />
-            <Route path='/written-off' element={withLazyLoadWrapper(<WrittenOffPage />)} />
+            <Route path='/warehouse' element={withLazyLoadWrapper(<WrittenOffPage />)} />
         </Routes>
     )
-
 }
 export const RouterMemo = withMemo(Router);
