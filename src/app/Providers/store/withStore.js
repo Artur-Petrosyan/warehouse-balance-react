06871847dc3b80e77401d18b4d05118f50d1860d
Artur <arturpetrosyan199808@gmail.com>
2023-12-22 11:44:58 +0400

    feat(app): store
    create 'store' for using 'redux',

---
+++ src/app/Providers/store/withStore.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import { rootReducer } from "./rootReducer";
+
+const store = configureStore({
+    reducer : rootReducer ,
+})
+
+export const withStore = ( component ) => <Provider store={store}>{component}</Provider>
