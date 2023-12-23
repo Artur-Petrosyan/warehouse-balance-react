ae64a866d8ffe815e9a34b115dad5842c993648e
Artur <arturpetrosyan199808@gmail.com>
2023-12-23 16:50:49 +0400

    feat(core/lib): return value
    change the 'error' return value

--- src/core/lib/XMLDataLocalStorage/getXMLDataFromLocalStorage.js
+++ src/core/lib/XMLDataLocalStorage/getXMLDataFromLocalStorage.js
@@ -1,7 +1,7 @@
 /**
  * Retrieves XML data from local storage and parses it using DOMParser.
  *
- * @param {Function} getItems - A function that retrieves items from the parsed XML data.
+ * @param {Function}  - A function that retrieves items from the parsed XML data.
  * @returns {void}
  *
  * @example
@@ -11,12 +11,13 @@
  * // Call the function with a function that retrieves items from the parsed XML data
  * getXMLDataFromLocalStorage(getItems);
  */
-export const getXMLDataFromLocalStorage = ( getItems ) => {
+export const getXMLDataFromLocalStorage = () => {
     let xmlData = localStorage.getItem('xmlData');
     if ( xmlData ) {
         let parser = new DOMParser();
         let xmlDoc = parser.parseFromString(xmlData , "text/xml");
         return xmlDoc.documentElement.children
+    }else {
+        return null
     }
-    return "Please upload the XML file"
 };
