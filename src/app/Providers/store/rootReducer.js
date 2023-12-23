a83d47d23f7a168d70dd5182ba65d55f47c6886d
Artur <arturpetrosyan199808@gmail.com>
2023-12-23 13:54:39 +0400

    feat(store): reducer
    add 'XMLData' reducer

--- src/app/Providers/store/rootReducer.js
+++ src/app/Providers/store/rootReducer.js
@@ -1,5 +1,7 @@
 import { combineReducers } from "@reduxjs/toolkit";
+import xmlDataReducer from "./xmlDataReducer";
 
 
 export const rootReducer = combineReducers({
+    XMLData: xmlDataReducer,
 });
