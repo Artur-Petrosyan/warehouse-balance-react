aa2a438a5a6ca243c999f293ccbd248aa00d24db
Artur <arturpetrosyan199808@gmail.com>
2023-12-23 11:50:23 +0400

    feat(pages): WrittenOffPage
    add 'table','button','upload'

--- src/pages/WrittenOff/ui/WrittenOffPage.jsx
+++ src/pages/WrittenOff/ui/WrittenOffPage.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Table } from "src/core/ui/components/Table/Table";
+import { Button , Upload } from "antd";
+import { getUploadProps } from "src/core/ui/components/Upload/lib";
 
 const WrittenOffController = () => {
+    const uploadProps = getUploadProps()
     return (
         <div>
-            WrittenOffController
+            <Table/>
+            <Button >Open File</Button>
+            <Upload {...uploadProps}><Button>Upload</Button></Upload>
         </div>
     );
 };
