8a529d3cded23aef1df41b63f677174a9f5f7f99
Artur <arturpetrosyan199808@gmail.com>
2023-12-23 16:52:28 +0400

    feat(ui): Table
    add the 'key' for 'columns'

--- src/core/ui/components/Table/Table.jsx
+++ src/core/ui/components/Table/Table.jsx
@@ -2,63 +2,40 @@ import { Table as TableAntd } from "antd";
 
 const columns = [
     {
+        key : '1' ,
         title : 'Name' ,
         dataIndex : 'name' ,
     } ,
     {
-        title : 'Quantity' ,
-        dataIndex : "quantity"
-    } ,
-    {
-        title : 'Without NDS' ,
-        dataIndex : 'Without NDS' ,
-    } ,
-    {
-        title : 'English Score' ,
-        dataIndex : 'english' ,
-        sorter : {
-            compare : ( a , b ) => a.english - b.english ,
-            multiple : 1 ,
-        } ,
-    } ,
-];
-const data = [
-    {
-        key : '1' ,
-        name : 'John Brown' ,
-        chinese : 98 ,
-        math : 60 ,
-        english : 70 ,
-    } ,
-    {
         key : '2' ,
-        name : 'Jim Green' ,
-        chinese : 98 ,
-        math : 66 ,
-        english : 89 ,
+        title : 'Quantity' ,
+        dataIndex : "quantity" ,
+
     } ,
     {
         key : '3' ,
-        name : 'Joe Black' ,
-        chinese : 98 ,
-        math : 90 ,
-        english : 70 ,
+
+        title : 'Without NDS' ,
+        dataIndex : 'withoutNDS' ,
+
     } ,
     {
         key : '4' ,
-        name : 'Jim Red' ,
-        chinese : 88 ,
-        math : 99 ,
-        english : 89 ,
+        title : 'Total Amount' ,
+        dataIndex : 'totalAmount' ,
+
     } ,
 ];
 const onChange = ( pagination , filters , sorter , extra ) => {
     console.log('params' , pagination , filters , sorter , extra);
 };
 
-export const Table = () => {
+export const Table = ( {dataSource} ) => {
     return (
-        <TableAntd columns={columns} dataSource={data} onChange={onChange}/>
+        <TableAntd
+            columns={columns}
+            dataSource={dataSource}
+            onChange={onChange}/>
     )
 }
 
