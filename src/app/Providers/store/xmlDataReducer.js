62911399499d513fff7ec6594c6d80b20ff20146
Artur <arturpetrosyan199808@gmail.com>
2023-12-25 16:41:18 +0400

    feat(store): thunk
    use the 'createAsyncThunk' for 'getData'

--- src/app/Providers/store/xmlDataReducer.js
+++ src/app/Providers/store/xmlDataReducer.js
@@ -1,15 +1,32 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk , createSlice } from '@reduxjs/toolkit';
+import { getXMLDataFromLocalStorage } from "src/core/lib/XMLDataLocalStorage/getXMLDataFromLocalStorage";
+import { arrayFromHTMLCollection } from "src/core/lib/arrayFromHTMLCollection";
+import { totalAmount } from "src/core/lib/totalAmount";
+
+export const setData = createAsyncThunk('XMLData/setData' , async () => {
+    const XMLData = await getXMLDataFromLocalStorage("Good");
+    const arrayFromXML = await arrayFromHTMLCollection(XMLData);
+    return totalAmount(arrayFromXML);
+});
 
 const xmlDataSlice = createSlice({
-    name : 'xmlData' ,
-    initialState : [] ,
-    reducers : {
-        setData : ( state , action ) => {
-            return action.payload
-        }
-    }
-})
-
-export const {setData} = xmlDataSlice.actions;
-// export const selectorXMLData = createSelector(( state ) => state.XMLData)
-export default xmlDataSlice.reducer
+    name : 'XMLData' ,
+    initialState : {
+        XMLData : [] ,
+        status : 'idle',
+    } ,
+    extraReducers : ( builder ) => {
+        builder
+            .addCase(setData.pending , ( state ) => {
+                state.status = 'loading';
+            })
+            .addCase(setData.fulfilled , ( state , action ) => {
+                state.status = 'succeeded';
+                state.XMLData = action.payload;
+            })
+            .addCase(setData.rejected , ( state ) => {
+                state.status = 'failed';
+            });
+    } ,
+});
+export default xmlDataSlice.reducer;
