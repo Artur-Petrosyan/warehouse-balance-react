b7eca0561f17e310dff0fa89caefbdbbed5452b3
Artur <arturpetrosyan199808@gmail.com>
2023-12-25 16:38:39 +0400

    docs(core/hoc): memo
    use 'JS Doc'

--- src/core/hoc/withMemo.jsx
+++ src/core/hoc/withMemo.jsx
@@ -1,3 +1,17 @@
 import { memo } from "react";
 
-export const withMemo = (component) => memo(component)
+/**
+ * Higher-order component that adds memoization to a component.
+ *
+ * @param {React.Component} component - The component to be memoized.
+ * @returns {React.Component} - The memoized component.
+ *
+ * @example
+ * import { withMemo } from "./withMemo";
+ * import MyComponent from "./MyComponent";
+ *
+ * const MemoizedComponent = withMemo(MyComponent);
+ *
+ * export default MemoizedComponent;
+ */
+export const withMemo = (component) => memo(component);
