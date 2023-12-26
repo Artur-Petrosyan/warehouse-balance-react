80834ce9b5278f0784e61018aa7704af50bcaf0c
Artur <arturpetrosyan199808@gmail.com>
2023-12-26 16:51:30 +0400

    feat(root): reducer
    add the new field 'EXCELData' in 'rootReducer'

--- src/app/Providers/store/rootReducer.js
+++ src/app/Providers/store/rootReducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import xmlDataReducer from "./xmlDataReducer";
+import excelDataReducer from "./excelDataReducer";
 
 
 export const rootReducer = combineReducers({
     XMLData: xmlDataReducer,
+    EXCELData: excelDataReducer
 });
