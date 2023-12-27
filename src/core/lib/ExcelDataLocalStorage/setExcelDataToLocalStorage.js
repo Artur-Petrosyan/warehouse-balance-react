59370664e5b3d2bc4ac69c6ff1f06ac836a6c5d4
Artur <arturpetrosyan199808@gmail.com>
2023-12-27 16:45:23 +0400

    feat(core/lib): excelLocalStorage
    create the function to set excelData to 'localStorage'

--- src/core/lib/ExcelDataLocalStorage/setExcelDataToLocalStorage.js
+++ src/core/lib/ExcelDataLocalStorage/setExcelDataToLocalStorage.js
@@ -1,3 +1,28 @@
-export const setExcelDataToLocalStorage = (file) => {
+/**
+ * Sets the Excel data from a file to local storage.
+ *
+ * @param {File} file - The file to read the Excel data from.
+ * @returns {boolean} false.
+ * @example
+ * import { setExcelDataToLocalStorage } from "path/to/setExcelDataToLocalStorage";
+ *
+ * const file = document.getElementById("excel-file").files[0];
+ * setExcelDataToLocalStorage(file);
+ */
+import * as XLSX from "xlsx";
+import { arrayWithOutFalsy } from "../arrayWithOutFalsy";
+import { createObjectFromArray } from "../createObjectFromArray";
 
+export const setExcelDataToLocalStorage = (file) => {
+    const reader = new FileReader();
+    reader.onload = async function (e) {
+        const excelData = e.target.result;
+        const workbook = XLSX.read(excelData, { type: 'binary' });
+        const sheets = workbook.Sheets[workbook.SheetNames[0]];
+        const jsonData = XLSX.utils.sheet_to_json(sheets, { header: 1 });
+        const arrayFromJsonData = arrayWithOutFalsy(jsonData);
+        localStorage.setItem('excelData', JSON.stringify(createObjectFromArray(arrayFromJsonData)));
+    };
+    reader.readAsBinaryString(file);
+    return false;
 };
