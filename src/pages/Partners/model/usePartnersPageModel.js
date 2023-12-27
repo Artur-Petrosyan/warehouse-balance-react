2050ece4765f614b01d81c94892169ffb4fb3943
Artur <arturpetrosyan199808@gmail.com>
2023-12-27 16:43:20 +0400

    feat(pages): Partners
    import

--- src/pages/Partners/model/usePartnersPageModel.js
+++ src/pages/Partners/model/usePartnersPageModel.js
@@ -1,6 +1,7 @@
 import { useCallback , useEffect } from "react";
 import { useDispatch , useSelector } from "react-redux";
 import { setExcelData } from "app/Providers/store/excelDataReducer";
+import { setExcelDataToLocalStorage } from "core/lib/ExcelDataLocalStorage";
 
 export const usePartnersPageModel = () => {
     const dispatch = useDispatch();
