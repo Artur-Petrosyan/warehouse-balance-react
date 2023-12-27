ecb1cc735f59666a5c22c17c84c26e550acd6e96
Artur <arturpetrosyan199808@gmail.com>
2023-12-27 16:51:24 +0400

    feat(ui): Table
    remove the 'colums' and add 'props'

--- src/core/ui/components/Table/Table.jsx
+++ src/core/ui/components/Table/Table.jsx
@@ -1,41 +1,10 @@
 import { Table as TableAntd } from "antd";
 
-const columns = [
-    {
-        key : '1' ,
-        title : 'Name' ,
-        dataIndex : 'name' ,
-    } ,
-    {
-        key : '2' ,
-        title : 'Unit' ,
-        dataIndex : "unit" ,
-
-    } ,
-    {
-        key : '3' ,
-        title : 'Quantity' ,
-        dataIndex : "amount" ,
-
-    } ,
-    {
-        key : '3' ,
-        title : 'Without NDS' ,
-        dataIndex : 'notNDS' ,
-
-    } ,
-    {
-        key : '3' ,
-        title : 'Total Amount' ,
-        dataIndex : 'totalPrice' ,
-
-    } ,
-];
 const onChange = ( pagination , filters , sorter , extra ) => {
     console.log('params' , pagination , filters , sorter , extra);
 };
 
-export const Table = ( {dataSource} ) => {
+export const Table = ( {dataSource , columns} ) => {
     return (
         <TableAntd
             columns={columns}
