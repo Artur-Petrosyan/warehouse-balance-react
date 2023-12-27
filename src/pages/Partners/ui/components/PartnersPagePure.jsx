f1bc5ac44af1d85413b4fac35490315254f431e6
Artur <arturpetrosyan199808@gmail.com>
2023-12-27 16:56:10 +0400

    feat(pages): Table columns
    create 'colums' for 'table'

--- src/pages/Partners/ui/components/PartnersPagePure.jsx
+++ src/pages/Partners/ui/components/PartnersPagePure.jsx
@@ -2,11 +2,31 @@ import React from 'react';
 import { Button } from "antd";
 import { Table , Upload } from "core";
 
-const PartnersPagePure = ( {openExcelFile , data} ) => {
+const columns = [
+    {
+        key : '1' ,
+        title : 'Name' ,
+        dataIndex : 'name' ,
+    } ,
+    {
+        key : '2' ,
+        title : 'HVHH' ,
+        dataIndex : "HVHH" ,
+
+    } ,
+    {
+        key : '3' ,
+        title : 'Address' ,
+        dataIndex : "address" ,
+
+    } ,
+];
+
+const PartnersPagePure = ( {openExcelFile , data , beforeUpload} ) => {
     return (
         <div>
-            <Table dataSource={data.EXCELData}/>
-            <Upload />
+            <Table columns={columns} dataSource={data.EXCELData}/>
+            <Upload beforeUpload={beforeUpload}/>
             <Button onClick={openExcelFile}>Open File</Button>
         </div>
     );
--- src/pages/WrittenOff/ui/components/WrittenOffPagePure.jsx
+++ src/pages/WrittenOff/ui/components/WrittenOffPagePure.jsx
@@ -2,11 +2,41 @@ import React , { memo } from "react";
 import { Button } from "antd";
 import { Table , Upload } from "core";
 import { withMemo } from "core";
+const columns = [
+    {
+        key : '1' ,
+        title : 'Name' ,
+        dataIndex : 'name' ,
+    } ,
+    {
+        key : '2' ,
+        title : 'Unit' ,
+        dataIndex : "unit" ,
 
+    } ,
+    {
+        key : '3' ,
+        title : 'Quantity' ,
+        dataIndex : "amount" ,
+
+    } ,
+    {
+        key : '3' ,
+        title : 'Without NDS' ,
+        dataIndex : 'notNDS' ,
+
+    } ,
+    {
+        key : '3' ,
+        title : 'Total Amount' ,
+        dataIndex : 'totalPrice' ,
+
+    } ,
+];
 export const WrittenOffPagePure = memo(( {data , openXmlFile , beforeUpload} ) => {
         return (
             <div>
-                <Table dataSource={data.XMLData}/>
+                <Table columns={columns} dataSource={data.XMLData}/>
                 <Upload beforeUpload={beforeUpload}/>
                 <Button onClick={openXmlFile}>Open File</Button>
             </div>
