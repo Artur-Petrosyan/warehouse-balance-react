bcee02ce4cb2506fb7ac0af8f7d7015d5d8140f6
Artur <arturpetrosyan199808@gmail.com>
2023-12-27 16:54:18 +0400

    feat(core/lib): get
    create the 'getExcelDataFromLocalStorage' function

---
+++ src/core/lib/ExcelDataLocalStorage/getExcelDataFromLocalStorage.js
@@ -0,0 +1,13 @@
+
+/**
+ * Retrieves the Excel data from local storage.
+ *
+ * @param {string} dataName - The name of the data to retrieve from local storage.
+ * @returns {Object} The Excel data from local storage.
+ * @example
+ * import { getExcelDataFromLocalStorage } from "path/to/getExcelDataFromLocalStorage";
+ *
+ * const excelData = getExcelDataFromLocalStorage("excelData");
+ * console.log(excelData); // Output: The Excel data from local storage
+ */
+export const getExcelDataFromLocalStorage = (dataName) => JSON.parse(localStorage.getItem(`${dataName}`));
