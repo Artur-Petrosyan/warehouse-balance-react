d809e8bba3a81d70c8cf29f9f44a15f661782810
Artur <arturpetrosyan199808@gmail.com>
2023-12-27 16:50:11 +0400

    feat(core/lib): remove falsy
    create the function for remove 'falsy' values from array

---
+++ src/core/lib/removeFalsyValuesFromArray.js
@@ -0,0 +1,13 @@
+/**
+ * Filters out any falsy values from an array and returns a new array with only truthy values.
+ *
+ * @param {Array} data - The array to filter.
+ * @returns {Array} A new array with only truthy values.
+ * @example
+ * import { arrayWithOutFalsy } from "path/to/arrayWithOutFalsy";
+ *
+ * const data = [0, 1, false, true, "", "hello", null, undefined];
+ * const filteredData = arrayWithOutFalsy(data);
+ * console.log(filteredData); // Output: [1, true, "hello"]
+ */
+export const removeFalsyValuesFromArray = (data) => data.map(item => item.filter(Boolean));
