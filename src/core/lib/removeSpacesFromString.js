e4cc054fd27b77323918bdc25c637d9cc6f8ee2e
Artur <arturpetrosyan199808@gmail.com>
2023-12-27 16:47:05 +0400

    feat(core/lib): removeSpaces
    create the function 'removeSpacesFromString'

---
+++ src/core/lib/removeSpacesFromString.js
@@ -0,0 +1,13 @@
+/**
+ * Removes spaces from a string and returns the modified string.
+ *
+ * @param {string} str - The string to remove spaces from.
+ * @returns {string} The modified string with spaces removed.
+ * @example
+ * import { removeSpacesFromString } from "path/to/removeSpacesFromString";
+ *
+ * const strWithSpaces = "This is a string with spaces";
+ * const strWithoutSpaces = removeSpacesFromString(strWithSpaces);
+ * console.log(strWithoutSpaces); // Output: "Thisisastringwithspaces"
+ */
+export const removeSpacesFromString = ( str ) => str.replace(/\s/g , '')
