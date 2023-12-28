148eaa804bff14679b96edf3f65a036694b0dfe4
Artur <arturpetrosyan199808@gmail.com>
2023-12-28 15:15:27 +0400

    feat(page): props
    set props in 'PartnersPagePure'

--- src/pages/Partners/ui/PartnersPage.jsx
+++ src/pages/Partners/ui/PartnersPage.jsx
@@ -3,11 +3,14 @@ import PartnersPagePure from "./components/PartnersPagePure";
 import { usePartnersPageModel } from "../model";
 
 const PartnersPage = () => {
-    const {data , openExcelFile , beforeUpload} = usePartnersPageModel()
+    const {data , searchData , searchQuery ,status, openExcelFile , beforeUpload} = usePartnersPageModel()
     /*TODO: create the Search logic for searching Partners  */
     return (
         <PartnersPagePure
             data={data}
+            searchData={searchData}
+            searchQuery={searchQuery}
+            status={status}
             openExcelFile={openExcelFile}
             beforeUpload={beforeUpload}
         />
