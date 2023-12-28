0cf75e1d931fe6a7c640c61508e0674b81ae9599
Artur <arturpetrosyan199808@gmail.com>
2023-12-28 15:12:59 +0400

    feat(hooks): props
    get the 'data' and add 'return'

--- src/pages/Partners/model/usePartnersPageModel.js
+++ src/pages/Partners/model/usePartnersPageModel.js
@@ -1,18 +1,56 @@
-import { useCallback , useEffect } from "react";
-import { useDispatch , useSelector } from "react-redux";
+/**
+ * A custom hook that provides data and functions for the partners page.
+ *
+ * @returns {Object} An object containing the following properties:
+ * - `data`: An array of partner data.
+ * - `searchData`: An array of filtered partner data based on a search query.
+ * - `searchQuery`: The current search query.
+ * - `status`: The status of the search query (loading, success, or error).
+ * - `openExcelFile`: A function that opens an Excel file and dispatches the `setExcelData` action to update the state with the new data.
+ * - `beforeUpload`: A function that sets the Excel data to local storage before uploading it.
+ *
+ * @example
+ * import React from 'eact';
+ * import { usePartnersPageModel } from './usePartnersPageModel';
+ *
+ * const PartnersPage = () => {
+ *   const { data, searchData, searchQuery, status, openExcelFile, beforeUpload } = usePartnersPageModel();
+ *
+ *   return (
+ *     <div>
+ *       <button onClick={openExcelFile}>Open Excel File</button>
+ *       <input type="file" accept=".xlsx" onChange={beforeUpload} />
+ *       {status === 'uccess' && (
+ *         <ul>
+ *           {searchData.map(partner => (
+ *             <li key={partner.id}>{partner.name}</li>
+ *           ))}
+ *         </ul>
+ *       )}
+ *     </div>
+ *   );
+ * };
+ *
+ * export default PartnersPage;
+ */
+import { useCallback, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { setExcelData } from "app/Providers/store/excelDataReducer";
 import { setExcelDataToLocalStorage } from "core/lib/ExcelDataLocalStorage";
 
 export const usePartnersPageModel = () => {
     const dispatch = useDispatch();
-    /*TODO: Add the logic when the user download not EXCEL file , "ALERT or ERROR" */
+    /*TODO: Add the logic when the user download not EXCEL file, "ALERT or ERROR" */
     const data = useSelector(( state ) => state.EXCELData);
+    const filteredData = useSelector(state => state.searchPartnersData)
+    const {searchData, searchQuery} = filteredData.searchData;
+    const status = filteredData.status
     useEffect(() => {
         dispatch(setExcelData())
-    } , [dispatch]);
+    }, [dispatch]);
     const beforeUpload = setExcelDataToLocalStorage
     const openExcelFile = useCallback(() => {
         dispatch(setExcelData())
-    } , [dispatch])
-    return {data , openExcelFile , beforeUpload};
-}
+    }, [dispatch])
+    return {data, searchData, searchQuery, status, openExcelFile, beforeUpload};
+};
