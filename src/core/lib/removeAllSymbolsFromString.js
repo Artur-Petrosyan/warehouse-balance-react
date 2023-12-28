09c0c8f2b0b17de10875e0940bb09abaa51d100a
Artur <arturpetrosyan199808@gmail.com>
2023-12-28 15:08:40 +0400

    feat(lib): string
    create function for remove symbols from 'string'

---
+++ src/core/lib/removeAllSymbolsFromString.js
@@ -0,0 +1,13 @@
+
+/**
+ * Removes all symbols from a string.
+ *
+ * @param {string} str - The string to remove symbols from.
+ * @returns {string} - The string with all symbols removed.
+ *
+ * @example
+ * const strWithSymbols = 'Hello<,./«» -"\' |@#$%^*()_+~`;:{}&?World';
+ * const strWithoutSymbols = removeAllSymbolsFromString(strWithSymbols);
+ * console.log(strWithoutSymbols); // 'HelloWorld'
+ */
+export const removeAllSymbolsFromString = (str) => str.replace(/[<,.>«» -"' |@#$%^*()_+~!՞՚։—՜՝`;:{}&?]/g, '');
