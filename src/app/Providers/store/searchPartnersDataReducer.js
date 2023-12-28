7e5597f6037c4bca3a6c8fa742ce41c2d1b1235d
Artur <arturpetrosyan199808@gmail.com>
2023-12-28 15:11:32 +0400

    feat(slice): store
    create the 'searchPartnersSlice'

---
+++ src/app/Providers/store/searchPartnersDataReducer.js
@@ -0,0 +1,36 @@
+import { createAsyncThunk , createSlice } from '@reduxjs/toolkit';
+import { getSearchPartnersData } from "pages/Partners/lib/getSearchPartnersData";
+
+export const setSearchPartnersData = createAsyncThunk(
+        'searchData/setSearchPartnersData' ,
+        async ( {data , query} ) => {
+            try {
+                return await {searchData : getSearchPartnersData(data , query) , searchQuery : query}
+            } catch (err) {
+                console.log(err)
+            }
+        }
+    )
+;
+
+const searchPartnersSlice = createSlice({
+    name : 'searchPartnersData' ,
+    initialState : {
+        searchData : [] ,
+        status : 'idle' ,
+    } ,
+    extraReducers : ( builder ) => {
+        builder
+            .addCase(setSearchPartnersData.pending , ( state ) => {
+                state.status = 'loading';
+            })
+            .addCase(setSearchPartnersData.fulfilled , ( state , action ) => {
+                state.status = 'succeeded';
+                state.searchData = action.payload
+            })
+            .addCase(setSearchPartnersData.rejected , ( state ) => {
+                state.searchData = 'failed';
+            });
+    } ,
+});
+export default searchPartnersSlice.reducer;
