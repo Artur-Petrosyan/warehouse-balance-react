14f8fd978e472194ba1e8af727beba25a59ebf1c
Artur <arturpetrosyan199808@gmail.com>
2023-12-28 15:09:50 +0400

    feat(pages): Partners Data
    create function,for 'filter' data by 'name'

---
+++ src/pages/Partners/lib/getSearchPartnersData.js
@@ -0,0 +1,23 @@
+/**
+ * Filters and returns an array of partner data based on a search query.
+ *
+ * @param {Array} data - An array of partner data objects.
+ * @param {string} query - The search query to filter the partner data by.
+ * @returns {Array} - An array of filtered partner data objects.
+ *
+ * @example
+ * const partnerData = [
+ *   { id: 1, name: 'John Doe' },
+ *   { id: 2, name: 'Jane Smith' },
+ *   { id: 3, name: 'Bob Johnson' },
+ * ];
+ *
+ * const filteredPartnerData = getSearchPartnersData(partnerData, 'Doe');
+ * console.log(filteredPartnerData); // [{ id: 1, name: 'John Doe' }]
+ */
+export const getSearchPartnersData = (data, query) => {
+    const regex = new RegExp(query);
+    if (data.length) {
+        return data.filter(item => regex.test(item.name));
+    }
+};
