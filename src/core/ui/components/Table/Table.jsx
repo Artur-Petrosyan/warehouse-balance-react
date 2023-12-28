861ce1e80fc8776acc0a5c97e97b45f024f57a34
Artur <arturpetrosyan199808@gmail.com>
2023-12-28 15:10:52 +0400

    feat(Table): Loader
    use 'status'

--- src/core/ui/components/Table/Table.jsx
+++ src/core/ui/components/Table/Table.jsx
@@ -1,15 +1,17 @@
 import { Table as TableAntd } from "antd";
+import { Loader } from "../Loader/Loader";
 
 const onChange = ( pagination , filters , sorter , extra ) => {
     console.log('params' , pagination , filters , sorter , extra);
 };
 
-export const Table = ( {dataSource , columns} ) => {
-    return (
-        <TableAntd
-            columns={columns}
-            dataSource={dataSource}
-            onChange={onChange}/>
-    )
+export const Table = ( {status , dataSource , columns} ) => {
+    return status === "loading" ? <Loader/> : <TableAntd
+        columns={columns}
+        dataSource={dataSource}
+        onChange={onChange}
+    />
+
+
 }
 
