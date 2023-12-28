10ef4334d6549d5d83ebe907684f396a4332d2d5
Artur <arturpetrosyan199808@gmail.com>
2023-12-28 15:16:39 +0400

    feat(ui): search
    use the 'dispatch','searchQuery','useEffect','useSelector'

--- src/core/ui/components/InputSearch.jsx
+++ src/core/ui/components/InputSearch.jsx
@@ -14,29 +14,59 @@
  *
  * export default inputSearch;
  */
-import {useState} from 'react'
-import {useNavigate} from 'react-router-dom'
-import {Input} from 'antd'
+import { useEffect, useState } from 'eact';
+import { Input } from 'antd';
+import { useDispatch, useSelector } from "react-redux";
+import { setSearchPartnersData } from "app/Providers/store/searchPartnersDataReducer";
+import { removeAllSymbolsFromString } from "src/core/lib/removeAllSymbolsFromString";
 
-const {Search} = Input
+const { Search } = Input;
 
-
+/**
+ * Renders an input search component with the given props.
+ *
+ * @returns {JSX.Element} The rendered input search component.
+ *
+ * @example
+ * import React from 'eact';
+ * import { InputSearch } from './InputSearch';
+ *
+ * const inputSearch = (
+ *   <InputSearch />
+ * );
+ *
+ * export default inputSearch;
+ */
 export const InputSearch = () => {
+    const dispatch = useDispatch();
+    const [searchQuery, setSearchQuery] = useState('');
+    const data = useSelector(state => state.EXCELData);
 
-    const [searchQuery, setSearchQuery] = useState('')
-    const navigate = useNavigate()
+    useEffect(() => {
+        if (searchQuery) {
+            const timeOutId = setTimeout(() => {
+                dispatch(setSearchPartnersData({
+                    data: data.EXCELData,
+                    query: removeAllSymbolsFromString(searchQuery),
+                }));
+            }, 1000);
+            return () => clearTimeout(timeOutId);
+        } else {
+            dispatch(setSearchPartnersData({
+                data: [],
+                query: searchQuery,
+            }));
+        }
+    }, [searchQuery, dispatch, data]);
 
     return (
         <div className='header__search'>
             <Search
-                placeholder="Find show"
+                placeholder="Find Partners"
                 allowClear
-                enterButton="Search"
-                onChange={(e) => setSearchQuery(e.target.value)}
-                onSearch={() => navigate(`search?q=${searchQuery}`)}
+                onChange={e => setSearchQuery(e.target.value)}
                 size="large"
-
             />
         </div>
-    )
-}
+    );
+};
