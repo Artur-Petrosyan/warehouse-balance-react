f3f70e1de88e72db5de61795c077dfbb7c5c8423
Artur-Petrosyan <arturpetrosyan199808@gmail.com>
2023-12-31 23:21:02 +0400

    fix(routes): lazy
    remove lazy loading from 'Home' page

--- src/app/Providers/routes.jsx
+++ src/app/Providers/routes.jsx
@@ -1,11 +1,12 @@
 import { Route , Routes } from "react-router-dom";
 
 import { lazy } from "react";
-import { withLazyLoadWrapper } from "core/lib/withLazyLoadWrapper";
+import { withLazyLoadWrapper } from "core/hoc/withLazyLoadWrapper";
 import { withMemo } from "core";
 import { Page404 } from "pages/Page404";
+import HomePage from "src/pages/Home/ui/HomePage";
 
-const HomePage = lazy(() => import('pages/Home/ui/HomePage'))
+
 const WrittenOffPage = lazy(() => import('pages/WrittenOff/ui/WrittenOffPage'))
 const AboutUsPage = lazy(() => import('pages/AboutUs/ui/AboutUsPage'))
 const ContactsPage = lazy(() => import('pages/Contacts/ui/ContactsPage'))
@@ -13,7 +14,7 @@ const PartnersPage = lazy(() => import('pages/Partners/ui/PartnersPage'))
 export const Router = () => {
     return (
         <Routes>
-            <Route index={true} element={withLazyLoadWrapper(<HomePage/>)}/>
+            <Route index={true} element={<HomePage />}/>
             <Route path='/warehouse' element={withLazyLoadWrapper(<WrittenOffPage/>)}/>
             <Route path='/about-us' element={withLazyLoadWrapper(<AboutUsPage/>)}/>
             <Route path='/contacts' element={withLazyLoadWrapper(<ContactsPage/>)}/>
