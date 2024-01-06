ffcf15ad2e6a7d0effa85f88cdb9fb1eb3cc2005
Artur-Petrosyan <arturpetrosyan199808@gmail.com>
2024-01-06 18:38:43 +0400

    feat(pages): alert message
    use the 'alertState' for show 'alert' message

--- src/pages/Partners/ui/components/PartnersPagePure.jsx
+++ src/pages/Partners/ui/components/PartnersPagePure.jsx
@@ -1,7 +1,6 @@
-import React from 'react';
-import { InputSearch , Table , Upload } from "core";
-import { Button } from "antd";
-import {withMemo} from "core";
+import React,  from 'react';
+import {InputSearch, Table, Upload, withMemo} from "core";
+import {Alert, Button} from "antd";
 
 const columns = [
     {
@@ -21,19 +20,37 @@ const columns = [
     } ,
 ];
 
-const PartnersPagePure = ( {data , searchData , searchQuery , status , openExcelFile , beforeUpload} ) => {
-    return (
-        <div>
-            <InputSearch placeholderText={"Find Partners"}/>
-            <Table
-                status={status}
-                columns={columns}
-                dataSource={searchQuery ? searchData : data.EXCELData}
-            />
-            < Upload beforeUpload={beforeUpload}/>
-            <Button onClick={openExcelFile}>Open File</Button>
-        </div>
-    );
-};
+const PartnersPagePure = ({
+         data,
+         searchData,
+         searchQuery,
+         status,
+         openExcelFile,
+         beforeUpload,
+         columns,
+         alertState
+     }
+    ) => {
+        return (
+            <div>
+                {alertState ? <Alert
+                    message="Warning"
+                    description="There is not a address"
+                    type="warning"
+                    showIcon
+                /> : null}
+                <InputSearch placeholderText={"Find Partners"}/>
+                <Table
+                    status={status}
+                    columns={columns}
+                    dataSource={searchQuery ? searchData : data.EXCELData}
+                />
+                < Upload beforeUpload={beforeUpload}/>
+                <Button onClick={openExcelFile}>Open File</Button>
+            </div>
+        );
+    };
 
-export const PartnersPagePureMemo = withMemo(PartnersPagePure) ;
+export const PartnersPagePureMemo = withMemo(PartnersPagePure);
+
+
