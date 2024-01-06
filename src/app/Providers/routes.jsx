44137ab089966b208dd72a102870597f6719829a
Artur-Petrosyan <arturpetrosyan199808@gmail.com>
2024-01-06 18:37:18 +0400

    feat(routes): route
    create the new route 'address' for 'PartnersAddressMapPage'

--- src/app/Providers/routes.jsx
+++ src/app/Providers/routes.jsx
@@ -1,24 +1,29 @@
-import { Route , Routes } from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 
-import { lazy } from "react";
-import { withLazyLoadWrapper } from "core/hoc/withLazyLoadWrapper";
-import { withMemo } from "core";
-import { Page404 } from "pages/Page404";
+import React, {lazy} from "react";
+import {withLazyLoadWrapper} from "core/hoc/withLazyLoadWrapper";
+import {withMemo} from "core";
+import {Page404} from "pages/Page404";
 import HomePage from "src/pages/Home/ui/HomePage";
-
 
 const WrittenOffPage = lazy(() => import('pages/WrittenOff/ui/WrittenOffPage'))
 const AboutUsPage = lazy(() => import('pages/AboutUs/ui/AboutUsPage'))
 const ContactsPage = lazy(() => import('pages/Contacts/ui/ContactsPage'))
 const PartnersPage = lazy(() => import('pages/Partners/ui/PartnersPage'))
+const PartnersAddressMapPage = lazy(() =>
+    import('pages/PartnersAddressMap/ui/PartnersAddressMapPage'))
+
 export const Router = () => {
     return (
         <Routes>
-            <Route index={true} element={<HomePage />}/>
+            <Route index={true} element={<HomePage/>}/>
             <Route path='/warehouse' element={withLazyLoadWrapper(<WrittenOffPage/>)}/>
             <Route path='/about-us' element={withLazyLoadWrapper(<AboutUsPage/>)}/>
             <Route path='/contacts' element={withLazyLoadWrapper(<ContactsPage/>)}/>
-            <Route path='/partners' element={withLazyLoadWrapper(<PartnersPage/>)}/>
+            <Route path='/partners'>
+                <Route index={true} element={withLazyLoadWrapper(<PartnersPage/>)} />
+                <Route path='address' element={withLazyLoadWrapper(<PartnersAddressMapPage/>)}/>
+            </Route>
             <Route path='*' element={<Page404/>}/>
         </Routes>
     )
