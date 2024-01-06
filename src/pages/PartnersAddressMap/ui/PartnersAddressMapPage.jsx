630d33904fa8f14959a3487ad9af88955c18e541
Artur-Petrosyan <arturpetrosyan199808@gmail.com>
2024-01-06 18:33:15 +0400

    feat(pages): google map
    create the 'PartnersAddressMapPage' for Google map

---
+++ src/pages/PartnersAddressMap/ui/PartnersAddressMapPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PartnersAddressMapPage = () => {
+    console.log("WORKED")
+    return (
+        <div>
+         <h1>PartnersAddressMapPage</h1>
+        </div>
+    );
+};
+
+export default PartnersAddressMapPage;
