a3af8d4210ffbdbd0e443e9f452067fea4382e25
Artur <arturpetrosyan199808@gmail.com>
2024-01-08 15:31:06 +0400

    feat(pages): hooks state
    use the 'setBuyerAndPriceData' for set 'data',
    and add the return new field 'buyerData'

--- src/pages/WrittenOff/model/useWrittenOffPageModel.jsx
+++ src/pages/WrittenOff/model/useWrittenOffPageModel.jsx
@@ -2,18 +2,21 @@ import { useCallback , useEffect } from "react";
 import { useDispatch , useSelector } from "react-redux";
 import { setXmlData } from "app/Providers/store/xmlDataReducer";
 import { setUploadXMLToLocalStorage } from "core/lib/XMLDataLocalStorage/setXMLDataReact";
+import { setBuyerAndPriceData } from "app/Providers/store/buyerAndPriceReducer";
 
 export const useWrittenOffPageModel = () => {
     const dispatch = useDispatch();
     /*TODO: Add the logic when the user download not XML file , "ALERT or ERROR" */
     const data = useSelector(( state ) => state.XMLData);
+    const buyerData = useSelector(( state ) => state.buyerAndPriceData.partnersAndPriceData);
     useEffect(() => {
         dispatch(setXmlData())
+        dispatch(setBuyerAndPriceData())
     } , [dispatch]);
 
     const beforeUpload = setUploadXMLToLocalStorage;
     const openXmlFile = useCallback(() => {
         dispatch(setXmlData())
     } , [dispatch])
-    return {data , openXmlFile , beforeUpload};
+    return {data ,buyerData, openXmlFile , beforeUpload};
 }
