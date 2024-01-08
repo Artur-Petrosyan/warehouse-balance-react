cdf47ab110a775b41e4922224c04f8d157ef3e5b
Artur <arturpetrosyan199808@gmail.com>
2024-01-08 15:28:28 +0400

    feat(reducer): root
    use the 'buyerAndPriceDataReducer' into 'rootReducer'

--- src/app/Providers/store/rootReducer.js
+++ src/app/Providers/store/rootReducer.js
@@ -2,10 +2,11 @@ import { combineReducers } from "@reduxjs/toolkit";
 import xmlDataReducer from "./xmlDataReducer";
 import excelDataReducer from "./excelDataReducer";
 import searchPartnersDataReducer from "./searchPartnersDataReducer";
-
+import buyerAndPriceReducer from "./buyerAndPriceReducer";
 
 export const rootReducer = combineReducers({
     XMLData : xmlDataReducer ,
     EXCELData : excelDataReducer ,
-    searchPartnersData : searchPartnersDataReducer
+    searchPartnersData : searchPartnersDataReducer ,
+    buyerAndPriceData : buyerAndPriceReducer
 });
