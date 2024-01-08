c6d58384c467e21c67adf5d0ae4f3196b171186f
Artur <arturpetrosyan199808@gmail.com>
2024-01-08 16:56:58 +0400

    feat(action): props

--- src/pages/WrittenOff/ui/WrittenOffPage.jsx
+++ src/pages/WrittenOff/ui/WrittenOffPage.jsx
@@ -3,13 +3,14 @@ import { useWrittenOffPageModel } from "../model";
 import { WrittenOffPagePureMemo } from "./components";
 
 const WrittenOffController = () => {
-    const {data ,buyerData, openXmlFile , beforeUpload} = useWrittenOffPageModel();
+    const {data ,buyerData, openXmlFile ,removeXMLData, beforeUpload} = useWrittenOffPageModel();
     return (
         <>
             <WrittenOffPagePureMemo
                 data={data}
                 buyerData={buyerData}
                 openXmlFile={openXmlFile}
+                removeXMLData={removeXMLData}
                 beforeUpload={beforeUpload}
             />
         </>
