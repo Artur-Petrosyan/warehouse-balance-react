5a854d13a4ac9f1344410b1b1445626bbc1ec9a5
Artur <arturpetrosyan199808@gmail.com>
2024-02-03 15:47:00 +0400

    feat(rootReducer): reducer
    use 'productListReducer' in 'rootReducer'

--- src/app/Providers/store/rootReducer.js
+++ src/app/Providers/store/rootReducer.js
@@ -3,10 +3,12 @@ import xmlDataReducer from "./xmlDataReducer";
 import excelDataReducer from "./excelDataReducer";
 import searchPartnersDataReducer from "./searchPartnersDataReducer";
 import buyerAndPriceReducer from "./buyerAndPriceReducer";
+import productListReducer from "./productListReducer";
 
 export const rootReducer = combineReducers({
     XMLData : xmlDataReducer ,
     EXCELData : excelDataReducer ,
     searchPartnersData : searchPartnersDataReducer ,
-    buyerAndPriceData : buyerAndPriceReducer
+    buyerAndPriceData : buyerAndPriceReducer ,
+    EXCELProductListData : productListReducer
 });
