7dec37b2e6f9b58fe29ffb2610683a60c2a7537f
Artur <arturpetrosyan199808@gmail.com>
2024-02-03 15:39:05 +0400

    feat(routes): new route
    add the new route '/product-list'

--- src/app/Providers/routes.jsx
+++ src/app/Providers/routes.jsx
@@ -10,6 +10,7 @@ const WrittenOffPage = lazy(() => import('pages/WrittenOff/ui/WrittenOffPage'))
 const AboutUsPage = lazy(() => import('pages/AboutUs/ui/AboutUsPage'))
 const ContactsPage = lazy(() => import('pages/Contacts/ui/ContactsPage'))
 const PartnersPage = lazy(() => import('pages/Partners/ui/PartnersPage'))
+const ProductListPage = lazy(() => import('pages/ProductList/ui/ProductListPage'))
 const PartnersAddressMapPage = lazy(() =>
     import('pages/PartnersAddressMap/ui/PartnersAddressMapPage'))
 
@@ -20,6 +21,7 @@ export const Router = () => {
             <Route path='/warehouse' element={withLazyLoadWrapper(<WrittenOffPage/>)}/>
             <Route path='/about-us' element={withLazyLoadWrapper(<AboutUsPage/>)}/>
             <Route path='/contacts' element={withLazyLoadWrapper(<ContactsPage/>)}/>
+            <Route path='/product-list' element={withLazyLoadWrapper(<ProductListPage />)} />
             <Route path='/partners'>
                 <Route index={true} element={withLazyLoadWrapper(<PartnersPage/>)} />
                 <Route path='address' element={withLazyLoadWrapper(<PartnersAddressMapPage/>)}/>
