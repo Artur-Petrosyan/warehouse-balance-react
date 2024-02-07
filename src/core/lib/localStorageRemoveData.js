48e67c5905f8380810557a130f9ebd0d5ca387df
Artur <arturpetrosyan199808@gmail.com>
2024-02-07 15:48:44 +0400

    feat(core): localStorage
    create the function for remove 'data' from 'localStorage'

---
+++ src/core/lib/localStorageRemoveData.js
@@ -0,0 +1,11 @@
+/**
+ * Removes data from local storage with the specified name.
+ *
+ * @function
+ * @param {string} dataName - The name of the data to remove from local storage.
+ *
+ * @example
+ * // Remove data from local storage with the name "myData"
+ * removeDataFromLocalStorage("myData");
+ */
+export const removeDataFromLocalStorage = (dataName) => localStorage.removeItem(`${dataName}`);
