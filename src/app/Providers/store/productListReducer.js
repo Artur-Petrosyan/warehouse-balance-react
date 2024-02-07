8ae2e9812259f59ec8237b45d9b34379de71d5fb
Artur <arturpetrosyan199808@gmail.com>
2024-02-07 15:51:57 +0400

    feat(store): reducer
    create the reducers for 'set' and 'remove' data

--- src/app/Providers/store/productListReducer.js
+++ src/app/Providers/store/productListReducer.js
@@ -1,32 +1,44 @@
-import { createAsyncThunk , createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk , createSlice } from "@reduxjs/toolkit";
 import { getExcelDataFromLocalStorage } from "core/lib/ExcelDataLocalStorage";
 
-export const setExcelProductListData = createAsyncThunk('EXCELProductListData/setExcelProductListData' , async () => {
-    try {
-        return await getExcelDataFromLocalStorage("excelProductListData")
-    } catch (err) {
-        console.log(err)
+export const setExcelProductListData = createAsyncThunk(
+    "EXCELProductListData/setExcelProductListData" ,
+    async () => {
+        try {
+            return await getExcelDataFromLocalStorage("excelProductListData");
+        } catch (err) {
+            console.log(err);
+        }
     }
-});
+);
 
 const excelProductListDataSlice = createSlice({
-    name : 'EXCELData' ,
+    name : "EXCELData" ,
     initialState : {
         EXCELProductListData : [] ,
-        status : 'idle' ,
+        status : "idle" ,
+    } ,
+    reducers : {
+        setProductListData : ( state , action ) => {
+            state.EXCELProductListData = action.payload;
+        } ,
+        removeProductListData : ( state ) => {
+            state.EXCELProductListData = [];
+        } ,
     } ,
     extraReducers : ( builder ) => {
         builder
             .addCase(setExcelProductListData.pending , ( state ) => {
-                state.status = 'loading';
+                state.status = "loading";
             })
             .addCase(setExcelProductListData.fulfilled , ( state , action ) => {
-                state.status = 'succeeded';
-                state.EXCELProductListData = action.payload;
+                state.status = "succeeded";
+                state.EXCELProductListData = action.payload || [];
             })
             .addCase(setExcelProductListData.rejected , ( state ) => {
-                state.EXCELProductListData = 'failed';
+                state.EXCELProductListData = "failed";
             });
     } ,
 });
 export default excelProductListDataSlice.reducer;
+export const {setProductListData , removeProductListData} = excelProductListDataSlice.actions;
