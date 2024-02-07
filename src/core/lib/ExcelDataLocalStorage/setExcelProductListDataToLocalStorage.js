fba00d1592a5f01eece711aa9900269e674a95f0
Artur <arturpetrosyan199808@gmail.com>
2024-02-07 15:59:09 +0400

    feat(lib): dispatch
    use the 'setProductListData' action

--- src/core/lib/ExcelDataLocalStorage/setExcelProductListDataToLocalStorage.js
+++ src/core/lib/ExcelDataLocalStorage/setExcelProductListDataToLocalStorage.js
@@ -1,7 +1,8 @@
-import { createObjectFromArray } from "../createObjectFromArray";
 import { removeFalsyValuesFromArray } from "../removeFalsyValuesFromArray";
 import * as XLSX from "xlsx";
 import { createObjectFromProductListArray } from "../createObjectFromProductListArray";
+import { store } from "app/Providers/store/withStore";
+import { setProductListData } from "app/Providers/store/productListReducer";
 
 /**
  * This function reads an Excel file and converts its data into a JSON object. It then removes any falsy values from the array and creates an object from the remaining values. Finally, it stores the object in local storage with the key 'excelProductListData'.
@@ -12,15 +13,19 @@ import { createObjectFromProductListArray } from "../createObjectFromProductList
  * const file = new File(['data:text/plain;base64,SGVsbG8gV29ybGQh'], 'example.txt', { type: 'text/plain' });
  * setExcelProductListDataToLocalStorage(file);
  */
-export const setExcelProductListDataToLocalStorage = (file) => {
+export const setExcelProductListDataToLocalStorage = ( file ) => {
     const reader = new FileReader();
-    reader.onload = async function (e) {
+    reader.onload = async function ( e ) {
         const excelData = e.target.result;
-        const workbook = XLSX.read(excelData, {type: 'binary'});
+        const workbook = XLSX.read(excelData , {type : "binary"});
         const sheets = workbook.Sheets[workbook.SheetNames[0]];
-        const jsonData = XLSX.utils.sheet_to_json(sheets, {header: 1});
+        const jsonData = XLSX.utils.sheet_to_json(sheets , {header : 1});
         const arrayFromJsonData = removeFalsyValuesFromArray(jsonData);
-        localStorage.setItem('excelProductListData', JSON.stringify(createObjectFromProductListArray(arrayFromJsonData)));
+        localStorage.setItem(
+            "excelProductListData" ,
+            JSON.stringify(createObjectFromProductListArray(arrayFromJsonData))
+        );
+        store.dispatch(setProductListData(createObjectFromProductListArray(arrayFromJsonData)));
     };
     reader.readAsBinaryString(file);
     return false;
