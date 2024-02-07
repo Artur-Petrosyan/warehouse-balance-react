98a13d092966c90d8871a3c2d1a91a7928a8f70e
Artur <arturpetrosyan199808@gmail.com>
2024-02-07 15:54:23 +0400

    refactor(function):change name

--- src/pages/WrittenOff/model/useWrittenOffPageModel.jsx
+++ src/pages/WrittenOff/model/useWrittenOffPageModel.jsx
@@ -1,22 +1,22 @@
 import { useCallback , useEffect } from "react";
 import { useDispatch , useSelector } from "react-redux";
-import { removeXML , setXmlData } from "app/Providers/store/xmlDataReducer";
+import { asyncSetXmlData , removeXML } from "app/Providers/store/xmlDataReducer";
 import { setUploadXMLToLocalStorage } from "core/lib/XMLDataLocalStorage/setXMLDataReact";
-import { setBuyerAndPriceData } from "app/Providers/store/buyerAndPriceReducer";
+import { asyncSetBuyerAndPriceData } from "app/Providers/store/buyerAndPriceReducer";
+import { removeBuyerAndPriceData } from "app/Providers/store/buyerAndPriceReducer";
 
 export const useWrittenOffPageModel = () => {
     const dispatch = useDispatch();
-    /*TODO: Add the logic when the user download not XML file , "ALERT or ERROR" */
     const data = useSelector(( state ) => state.XMLData);
     const buyerData = useSelector(( state ) => state.buyerAndPriceData.partnersAndPriceData);
-
     useEffect(() => {
-        dispatch(setXmlData())
-        dispatch(setBuyerAndPriceData())
+        dispatch(asyncSetXmlData())
+        dispatch(asyncSetBuyerAndPriceData())
     } , [dispatch]);
 
     const removeXMLData = useCallback(() => {
         dispatch(removeXML())
+        dispatch(removeBuyerAndPriceData())
     } , [dispatch])
 
     const beforeUpload = setUploadXMLToLocalStorage;
