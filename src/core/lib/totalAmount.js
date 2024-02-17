bcac1dc122b45e38e3ddb67367e22db9960dbd68
Artur <arturpetrosyan199808@gmail.com>
2024-02-17 09:44:57 +0400

    fix(err): total Nds
    change the 'obj' to 'item'

--- src/core/lib/totalAmount.js
+++ src/core/lib/totalAmount.js
@@ -43,7 +43,7 @@ export const totalAmount = data => data.reduce(( acc , obj ) => {
     return acc.map(( item ) => {
         return {
             ...item ,
-            notNDS : Number(obj?.notNDS.toFixed(2)) ,
+            notNDS : Number(item?.notNDS.toFixed(2)) ,
             totalPrice : Number(item?.totalPrice.toFixed(2))
         }
     });
