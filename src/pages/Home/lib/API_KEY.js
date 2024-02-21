a8fa04bd2b52e8d251b3b4671e0520161757aaae
Artur <arturpetrosyan199808@gmail.com>
2024-02-21 16:14:29 +0400

    feat(): .gitignore
    add the "API_KEY"

---
+++ src/pages/Home/lib/API_KEY.js
@@ -0,0 +1,1 @@
+export const API_KEY = "d66b181827mshf97181d3a8359fdp103386jsn32f89a2cba7b";
--- src/pages/Home/lib/fetchGetExchangeData.js
+++ src/pages/Home/lib/fetchGetExchangeData.js
@@ -1,9 +1,11 @@
+import { API_KEY } from "./API_KEY";
+
 export const fetchGetExchangeRate = async () => {
   const url = `https://currency-exchange.p.rapidapi.com/exchange?from=KWD&to=USD`;
   const options = {
     method: "GET",
     headers: {
-      "X-RapidAPI-Key": "d66b181827mshf97181d3a8359fdp103386jsn32f89a2cba7b",
+      "X-RapidAPI-Key": `${API_KEY}`,
       "X-RapidAPI-Host": "currency-exchange.p.rapidapi.com",
     },
   };
