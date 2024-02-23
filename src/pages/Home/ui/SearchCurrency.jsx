46ea6f2d8b48fda6891e8cbba9f51621b91bf283
Artur <arturpetrosyan199808@gmail.com>
2024-02-23 15:49:55 +0400

    feat(SearchCurrency): Flex
    use 'Flex'  component,
    set 'initialValues',
    set 'virtual' false,
    set 'defaultValue',
    set 'currency' length

--- src/pages/Home/ui/SearchCurrency.jsx
+++ src/pages/Home/ui/SearchCurrency.jsx
@@ -1,7 +1,8 @@
-import { Select, Typography } from "antd";
+import { Flex, Select, Typography } from "antd";
 import React from "react";
+import Flags from "./Flags";
 
-export const SelectSearchCurrency = ({ searchCurrency, currencyData, onChange, defaultValue }) => {
+export const SelectSearchCurrency = ({ initialValue, searchCurrency, currencyData, onChange }) => {
     return (
         <Select
             showSearch
@@ -10,19 +11,24 @@ export const SelectSearchCurrency = ({ searchCurrency, currencyData, onChange, d
                 marginBottom: 20
             }}
             onChange={onChange}
+            virtual={false}
             placeholder="Type to Search"
             optionFilterProp="children"
+            optionLabelProp="children"
             filterOption={searchCurrency}
-            defaultValue={defaultValue}
+            defaultValue={initialValue}
             filterSort={(optionA, optionB) =>
                 (optionA?.label ?? "").toLowerCase().localeCompare((optionB?.label ?? "").toLowerCase())
             }
         >
             {currencyData.map(({ code, currency }) => (
-                <Select.Option key={code} value={code}>
-                    <Typography.Text style={{ fontSize: 20 }}>
-                        {code} - {currency}
-                    </Typography.Text>
+                <Select.Option key={code} value={code} maxTagTextLength={10}>
+                    <Flex align={"center"} style={{ gap: 10 }}>
+                        <Flags currency={code} />
+                        <Typography.Text style={{ fontSize: 16 }} strong={true}>
+                            {currency.length > 24 ? `${code} - ${currency.slice(0, 24)}...` : `${code} - ${currency}`}
+                        </Typography.Text>
+                    </Flex>
                 </Select.Option>
             ))}
         </Select>
