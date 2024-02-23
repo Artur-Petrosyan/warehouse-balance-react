c23101728a404b68568633349568f713747e914e
Artur <arturpetrosyan199808@gmail.com>
2024-02-23 15:47:32 +0400

    fix(err): currency
    change "code" to "currency"

--- src/pages/Home/ui/Flags.jsx
+++ src/pages/Home/ui/Flags.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import CurrencyFlagImage from "react-currency-flags/dist/components";
 
-const Flags = ({ code }) => {
-    return <CurrencyFlagImage currency={code} width={30} />;
+const Flags = ({ currency }) => {
+    return <CurrencyFlagImage currency={currency} width={30} />;
 };
 
 export default Flags;
