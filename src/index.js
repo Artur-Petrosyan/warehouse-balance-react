4136cd969234c3d81deff23cf3bd98945bc28527
Artur <arturpetrosyan199808@gmail.com>
2024-02-24 16:43:00 +0400

    feat(plugin): dependencies
    install the 'react buddy' plugin, and dependencies  "@react-buddy/ide-toolbox","@react-buddy/palette-antd",

--- src/index.js
+++ src/index.js
@@ -5,8 +5,14 @@ import App from "./app/App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { withStore } from "./app/Providers/store/withStore";
+import { DevSupport } from "@react-buddy/ide-toolbox";
+import { ComponentPreviews , useInitial } from "./dev";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<BrowserRouter>{withStore(<App />)}</BrowserRouter>);
+root.render(<BrowserRouter>{withStore(<DevSupport ComponentPreviews={ComponentPreviews}
+                                                  useInitialHook={useInitial}
+>
+    <App/>
+</DevSupport>)}</BrowserRouter>);
 
 reportWebVitals();
