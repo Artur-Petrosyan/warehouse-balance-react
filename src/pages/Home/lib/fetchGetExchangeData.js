b85fd53114066b7ebbbe2967db42b5ee24fcb0f0
Artur <arturpetrosyan199808@gmail.com>
2024-02-26 11:50:09 +0400

    feat(fetch): error handling

--- src/pages/Home/lib/fetchGetExchangeData.js
+++ src/pages/Home/lib/fetchGetExchangeData.js
@@ -1,6 +1,6 @@
 import { API_KEY } from "./API_KEY";
 
-export const fetchGetExchangeRate = async (from, to, setIsLoading) => {
+export const fetchGetExchangeRate = async (from, to, setIsLoading, setError) => {
     const url = `https://currency-exchange.p.rapidapi.com/exchange?from=${from}&to=${to}`;
     const options = {
         method: "GET",
@@ -12,10 +12,13 @@ export const fetchGetExchangeRate = async (from, to, setIsLoading) => {
     try {
         setIsLoading(true);
         const response = await fetch(url, options);
+        if (response.status === 504) throw new Error("The server is currently unavailable. Please try again later.");
         const data = await response.json();
         setIsLoading(false);
         return data;
     } catch (error) {
         console.error(error);
+        setError(error.message);
+        setIsLoading(false);
     }
 };
