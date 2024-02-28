7704f7238ba1a555be3ae43ec92b64abdcc2e674
Artur <arturpetrosyan199808@gmail.com>
2024-02-28 14:11:17 +0400

    feat(Provider): Config Provider

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import LayoutMemo from "../core/ui/components/Layout/Layout";
 import "../App.css";
-
+import { ConfigProvider } from "antd";
+import styles from "src/core/styles.json";
 function App() {
     return (
-        <div className="App">
-            <LayoutMemo />
-        </div>
+        <ConfigProvider theme={styles}>
+            <div className="App">
+                <LayoutMemo />
+            </div>
+        </ConfigProvider>
     );
 }
 
