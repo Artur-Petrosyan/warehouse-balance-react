86480aa14aa1dbbab99b301c62b12498344554a4
Artur-Petrosyan <arturpetrosyan199808@gmail.com>
2024-03-24 14:26:15 +0400

    Worked

--- src/core/ui/components/Button.jsx
+++ src/core/ui/components/Button.jsx
@@ -30,6 +30,7 @@
  * }
  */
 export const Button = ({ children, variant = "", ...rest }) => {
+    console.log("WORKED");
     return (
         <button {...rest} className={`button ${variant}`}>
             {children}
